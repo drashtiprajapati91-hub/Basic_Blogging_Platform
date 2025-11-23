7b00da7fa7c8d9e5c2805edc38d8cb5567af0e98
Sahil Chauhan <sahilchauhan99041@gmail.com>
2025-11-23 15:00:45 +0530

    Initial commit

---
+++ backend/routes/postRoutes.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+// Get all posts
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Create post
+router.post('/', async (req, res) => {
+    const { title, content } = req.body;
+    const post = new Post({ title, content });
+    try {
+        const savedPost = await post.save();
+        res.status(201).json(savedPost);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Update post
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedPost);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Delete post
+router.delete('/:id', async (req, res) => {
+    try {
+        await Post.findByIdAndDelete(req.params.id);
+        res.json({ message: 'Post deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+module.exports = router;
